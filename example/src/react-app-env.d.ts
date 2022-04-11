181c87c1a263068eb18bb953bf71d22b34e22b92
Huynh Duc Dung <dunghd.it@gmail.com>
2022-04-11 20:23:58 +0800

    :arrow_up: Update react 18

--- example/src/react-app-env.d.ts
+++ example/src/react-app-env.d.ts
@@ -50,9 +50,9 @@ declare module '*.webp' {
 declare module '*.svg' {
   import * as React from 'react'
 
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+  export const ReactComponent: React.FunctionComponent<React.PropsWithChildren<React.SVGProps<
     SVGSVGElement
-  > & { title?: string }>
+  > & { title?: string }>>
 
   const src: string;
   export default src
